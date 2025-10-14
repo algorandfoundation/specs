a0d7349d8ca73cfddec1fb4f9cffa916224bc00a
Larkin Young <larkin.young@algorand.foundation>
2025-10-14 09:46:06 +0100

    Integrated Plausible Analytics

---
+++ theme/gtm-noscript.js
@@ -0,0 +1,12 @@
+// theme/gtm-noscript.js
+(function () {
+  var noscript = document.createElement("noscript");
+  var iframe = document.createElement("iframe");
+  iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-NZL38PG9";
+  iframe.height = "0";
+  iframe.width = "0";
+  iframe.style.display = "none";
+  iframe.style.visibility = "hidden";
+  noscript.appendChild(iframe);
+  document.body.insertBefore(noscript, document.body.firstChild);
+})();
